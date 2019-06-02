5d1015e06f8c62e416f3ece21d12e08cdb959e0b
Svein Løvmoen Ellingsen <sle@gamefraym.com>
2019-06-02 15:10:19 +0200

    Async command execution w/wait

--- mysender/MyMessageCommand.cs
+++ mysender/MyMessageCommand.cs
@@ -4,20 +4,32 @@ using Oakton;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace mysender
 {
     public class MyMessageCommand : OaktonCommand<JasperInput>
     {
+        private static ManualResetEvent _event = new ManualResetEvent(false);
+
         public override bool Execute(JasperInput input)
+        {
+            ExecuteAsync(input);
+            _event.WaitOne();
+
+            return true;
+        }
+
+        private static async void ExecuteAsync(JasperInput input)
         {
             using (var runtime = input.BuildHost(StartMode.Full))
             {
                 var m = new MyMessage { Id = Guid.NewGuid(), Message = "Test message" };
-                runtime.Messaging.Publish(m);
+                await runtime.Messaging.Publish(m);
+                await Task.Delay(1000);
+                _event.Set();
             }
-
-            return true;
         }
     }
 }
